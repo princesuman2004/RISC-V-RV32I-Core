047424d16c69a84b3e77e6a21841d5bedee0b474
Prince Suman <128327318+princesuman2004@users.noreply.github.com>
2023-07-29 11:24:11 +0530

    Update processor.v

--- processor.v
+++ processor.v
@@ -1,216 +1,306 @@
 `timescale 1ns / 1ps
- //////////////////////////////////////////////////////////////////////////////////
- // Company: 
- // Engineer: @princesuman
- // 
- // Create Date: 09.06.2023 06:59:19
- // Design Name: 
- // Module Name: Microprocessor_Design
- // Project Name: Microprocessor_Design
- // Target Devices: 
- // Tool Versions: 
- // Description: 
- // 
- // Dependencies: 
- // 
- // Revision:
- // Revision 0.01 - File Created
- // Additional Comments:
- // 
- //////////////////////////////////////////////////////////////////////////////////
  
- //defining the name to subparts of IR
- `define oper_type IR[31:27]
- `define rdst      IR[26:22]
- `define rsrc1     IR[21:17]
- `define addmode   IR[16]
- `define rsrc2     IR[15:11]
- `define isrc      IR[15:0]
+///////////fields of IR
+`define oper_type IR[31:27]
+`define rdst      IR[26:22]
+`define rsrc1     IR[21:17]
+`define imm_mode  IR[16]
+`define rsrc2     IR[15:11]
+`define isrc      IR[15:0]
  
  
- //defining the codes to arithmetic operations >> opcode>>
- `define movsgpr  5'b00000
- `define mov      5'b00001
- `define add      5'b00010
- `define sub      5'b00011
- `define mul      5'b00100
-  
-  //defining the codes to logical operations >> opcode>>
-  `define rand   5'b00101
-  `define ror    5'b00110
-  `define rnot   5'b00111
-  `define rnand  5'b01000
-  `define rnor   5'b01001
-  `define rxor   5'b01010
-  `define rxnor  5'b01011
+////////////////arithmetic operation
+`define movsgpr        5'b00000
+`define mov            5'b00001
+`define add            5'b00010
+`define sub            5'b00011
+`define mul            5'b00100
  
- module processor();
+////////////////logical operations : and or xor xnor nand nor not
  
-     reg [31:0]IR;             //main IR 
-     reg [15:0] GPR[31:0];    // 32 registers of 16 bit 
-     reg [15:0] SGPR;          
-     reg [31:0] mul_temp;
-     
-         always @ (*)
+`define ror            5'b00101
+`define rand           5'b00110
+`define rxor           5'b00111
+`define rxnor          5'b01000
+`define rnand          5'b01001
+`define rnor           5'b01010
+`define rnot           5'b01011
+ 
+/////////////////////// load & store instructions
+ 
+`define storereg       5'b01101   //////store content of register in data memory
+`define storedin       5'b01110   ////// store content of din bus in data memory
+`define senddout       5'b01111   /////send data from DataMemory to dout bus
+`define sendreg        5'b10001   ////// send data from DM to register
+ 
+ 
+ 
+module top(
+input clk,
+input [31:0] din,
+//input [31:0] instructions,
+output reg [15:0] dout
+);
+integer i=0;
+ 
+////////////////adding program and data memory
+reg [31:0] inst_mem [15:0]; ////program memory
+reg [15:0] data_mem [15:0]; ////data memory
+ 
+ 
+ 
+ 
+ 
+reg [31:0]IR;            ////// instruction register  <--ir[31:27]--><--ir[26:22]--><--ir[21:17]--><--ir[16]--><--ir[15:11]--><--ir[10:0]-->
+                          //////fields                 <---  oper  --><--   rdest --><--   rsrc1 --><--modesel--><--  rsrc2 --><--unused  -->             
+                          //////fields                 <---  oper  --><--   rdest --><--   rsrc1 --><--modesel--><--  immediate_date      -->   2^15   
+ 
+reg [15:0] GPR [31:0] ;   ///////general purpose register gpr[0] ....... gpr[31]
+ 
+ 
+ 
+reg [15:0] SGPR ;      ///// msb of multiplication --> special register
+ 
+reg [31:0] mul_res;
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+//task decode_inst();
+always@(*)
+begin
+case(`oper_type)
+///////////////////////////////
+`movsgpr: begin
+ 
+   GPR[`rdst] = SGPR;
+   
+end
+ 
+/////////////////////////////////
+`mov : begin
+   if(`imm_mode)
+        GPR[`rdst]  = `isrc;
+   else
+       GPR[`rdst]   = GPR[`rsrc1];
+end
+ 
+////////////////////////////////////////////////////
+ 
+`add : begin
+      if(`imm_mode)
+        GPR[`rdst]   = GPR[`rsrc1] + `isrc;
+     else
+        GPR[`rdst]   = GPR[`rsrc1] + GPR[`rsrc2];
+end
+ 
+/////////////////////////////////////////////////////////
+ 
+`sub : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] - `isrc;
+     else
+       GPR[`rdst]   = GPR[`rsrc1] - GPR[`rsrc2];
+end
+ 
+/////////////////////////////////////////////////////////////
+ 
+`mul : begin
+      if(`imm_mode)
+        mul_res   = GPR[`rsrc1] * `isrc;
+     else
+        mul_res   = GPR[`rsrc1] * GPR[`rsrc2];
+        
+     GPR[`rdst]   =  mul_res[15:0];
+     SGPR         =  mul_res[31:16];
+end
+ 
+///////////////////////////////////////////////////////////// bitwise or
+ 
+`ror : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] | `isrc;
+     else
+       GPR[`rdst]   = GPR[`rsrc1] | GPR[`rsrc2];
+end
+ 
+////////////////////////////////////////////////////////////bitwise and
+ 
+`rand : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] & `isrc;
+     else
+       GPR[`rdst]   = GPR[`rsrc1] & GPR[`rsrc2];
+end
+ 
+//////////////////////////////////////////////////////////// bitwise xor
+ 
+`rxor : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] ^ `isrc;
+     else
+       GPR[`rdst]   = GPR[`rsrc1] ^ GPR[`rsrc2];
+end
+ 
+//////////////////////////////////////////////////////////// bitwise xnor
+ 
+`rxnor : begin
+      if(`imm_mode)
+        GPR[`rdst]  = GPR[`rsrc1] ~^ `isrc;
+     else
+        GPR[`rdst]   = GPR[`rsrc1] ~^ GPR[`rsrc2];
+end
+ 
+//////////////////////////////////////////////////////////// bitwisw nand
+ 
+`rnand : begin
+      if(`imm_mode)
+        GPR[`rdst]  = ~(GPR[`rsrc1] & `isrc);
+     else
+       GPR[`rdst]   = ~(GPR[`rsrc1] & GPR[`rsrc2]);
+end
+ 
+////////////////////////////////////////////////////////////bitwise nor
+ 
+`rnor : begin
+      if(`imm_mode)
+        GPR[`rdst]  = ~(GPR[`rsrc1] | `isrc);
+     else
+       GPR[`rdst]   = ~(GPR[`rsrc1] | GPR[`rsrc2]);
+end
+ 
+////////////////////////////////////////////////////////////not
+ 
+`rnot : begin
+      if(`imm_mode)
+        GPR[`rdst]  = ~(`isrc);
+     else
+        GPR[`rdst]   = ~(GPR[`rsrc1]);
+end
+ 
+////////////////////////////////////////////////////////////
+ 
+`storedin: begin
+   data_mem[`isrc] = din;
+end
+ 
+/////////////////////////////////////////////////////////////
+ 
+`storereg: begin
+   data_mem[`isrc] = GPR[`rsrc1];
+end
+ 
+/////////////////////////////////////////////////////////////
+ 
+ 
+`senddout: begin
+   dout  = data_mem[`isrc]; 
+end
+ 
+/////////////////////////////////////////////////////////////
+ 
+`sendreg: begin
+  GPR[`rdst] =  data_mem[`isrc];
+end
+ 
+/////////////////////////////////////////////////////////////
+endcase
+end
+
+ 
+ 
+ 
+///////////////////////logic for condition flag
+reg sign = 0, zero = 0, overflow = 0, carry = 0;
+reg [16:0] temp_sum;
+ 
+//task decode_condflag();
+always@(*)
+begin
+ 
+/////////////////sign bit
+if(`oper_type == `mul)
+  sign = SGPR[15];
+else
+  sign = GPR[`rdst][15];
+ 
+////////////////carry bit
+ 
+if(`oper_type == `add)
+   begin
+      if(`imm_mode)
          begin
-         case(`oper_type)
-            `movsgpr:begin 
-             GPR[`rdst]=SGPR;
-             end
-             //////////
-             `mov:begin 
-             GPR[`rdst]=GPR[`rsrc1];
-              end
-              /////////
-             `add:begin
-             if(`addmode)
-              GPR[`rdst]=GPR[`rsrc1]+`isrc;
-             else
-              GPR[`rdst]=GPR[`rsrc1]+GPR[`rsrc2];
-              
-              end
-              //////////
-             `sub:begin
-             if(`addmode)
-              GPR[`rdst]=GPR[`rsrc1]-`isrc;
-             else
-              GPR[`rdst]=GPR[`rsrc1]-GPR[`rsrc2]; 
-              end
-              //////////
-              `mul:begin 
-                  if(`addmode)
-                  begin
-                  mul_temp=GPR[`rsrc1]*`isrc;
-                  GPR[`rdst]=mul_temp[15:0];
-                 SGPR=mul_temp[31:16];
-                 end
-                   
-                  else
-                  begin
-                  mul_temp=GPR[`rsrc1]*GPR[`rsrc2];
-                  
-                 GPR[`rdst]=mul_temp[15:0];
-                 SGPR=mul_temp[31:16];
-                 end
-               end
-               ////////
-               `rand:begin
-               if(`addmode)
-              GPR[`rdst]=GPR[`rsrc1]&`isrc;
-             else
-              GPR[`rdst]=GPR[`rsrc1]& GPR[`rsrc2];
-              end
-              /////
-              `ror:begin
-               if(`addmode)
-              GPR[`rdst]=GPR[`rsrc1]|`isrc;
-             else
-              GPR[`rdst]=GPR[`rsrc1]|GPR[`rsrc2];
-              end
-              ///
-              `rnot:begin
-               if(`addmode)
-              GPR[`rdst]=~(`isrc);
-             else
-              GPR[`rdst]=~(GPR[`rsrc1]);
-              end
-              ////
-               `rnand:begin
-              if(`addmode)
-                GPR[`rdst]= ~(GPR[`rsrc1]&`isrc);
-              else
-                GPR[`rdst]=~(GPR[`rsrc1]&GPR[`rsrc2]);
-              end
-              ////
-              `rnor:begin
-              if(`addmode)
-                GPR[`rdst]= ~(GPR[`rsrc1]|`isrc);
-              else
-                GPR[`rdst]=~(GPR[`rsrc1]|GPR[`rsrc2]);
-              end
-              ////
-              
-              `rxnor:begin
-              if(`addmode)
-                GPR[`rdst]= ~(GPR[`rsrc1]^`isrc);
-              else
-                GPR[`rdst]=~(GPR[`rsrc1]^GPR[`rsrc2]);
-              end
-              ////
-              `rxor:begin
-              if(`addmode)
-                GPR[`rdst]= GPR[`rsrc1]^`isrc;
-              else
-                GPR[`rdst]=GPR[`rsrc1]^GPR[`rsrc2];
-              end
-              ////
-              
-              ////
-              
-               
-             endcase //end of case
-             //////
-             end //end of always block 
-             
- //logic for setting flags
+         temp_sum = GPR[`rsrc1] + `isrc;
+         carry    = temp_sum[16]; 
+         end
+      else
+         begin
+         temp_sum = GPR[`rsrc1] + GPR[`rsrc2];
+         carry    = temp_sum[16]; 
+         end   end
+   else
+    begin
+        carry  = 1'b0;
+    end
  
- reg zero =0,carry=0,sign=0,overflow=0,parity=0;  //parity to be added later
- reg [16:0]add_temp;
-  always @(*) 
-  begin
-  ///////////////
-  //carry
-  if(`oper_type==`add)
-  begin
-  if(`addmode)
-  begin
-  add_temp=GPR[`rsrc1]+`isrc;
-  carry=add_temp[16];
-  end 
-  else
-  begin
-  add_temp=GPR[`rsrc1]+GPR[`rsrc2];
-  carry=add_temp[16];
-  end 
-  end
-  else
-  carry=1'b0;  // carry=0; difference?
-  ///////////////
-  //sign
-  if(`oper_type==`mul)
-  sign=SGPR[15];
-  else
-  sign=GPR[`rdst][15];
-  ////////////////
-  //zero
-  if(`oper_type==`mul)
-  zero=~((|GPR[`rdst])&(|SGPR[15:0]));
-  else
-  zero=~(|GPR[`rdst]);
-  
-  /////////////////
-  //overflow
-  if(`oper_type==`add)
-  begin
-  if(`addmode)
-overflow=((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(~IR[15]))|~((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(~IR[15])) ;
-  else
-overflow=((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(~GPR[`rsrc2][15]))|~((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(~GPR[`rsrc2][15])) ;
+///////////////////// zero bit
  
-  end
-  else if(`oper_type==`sub)
-  begin
-  if(`addmode)
-  overflow=((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(IR[15]))|((~GPR[`rdst][15])&(GPR[`rsrc1][15])&(~IR[15])) ;
+ zero =  ( ~(|GPR[`rdst]) | ~(|SGPR[15:0]) )  ; 
+ 
+ 
+//////////////////////overflow bit
+ 
+if(`oper_type == `add)
+     begin
+       if(`imm_mode)
+         overflow = ( (~GPR[`rsrc1][15] & ~IR[15] & GPR[`rdst][15] ) | (GPR[`rsrc1][15] & IR[15] & ~GPR[`rdst][15]) );
+       else
+         overflow = ( (~GPR[`rsrc1][15] & ~GPR[`rsrc2][15] & GPR[`rdst][15]) | (GPR[`rsrc1][15] & GPR[`rsrc2][15] & ~GPR[`rdst][15]));
+     end
+  else if(`oper_type == `sub)
+    begin
+       if(`imm_mode)
+         overflow = ( (~GPR[`rsrc1][15] & IR[15] & GPR[`rdst][15] ) | (GPR[`rsrc1][15] & ~IR[15] & ~GPR[`rdst][15]) );
+       else
+         overflow = ( (~GPR[`rsrc1][15] & GPR[`rsrc2][15] & GPR[`rdst][15]) | (GPR[`rsrc1][15] & ~GPR[`rsrc2][15] & ~GPR[`rdst][15]));
+    end 
   else
-  overflow=((GPR[`rdst][15])&(~GPR[`rsrc1][15])&(GPR[`rsrc2][15]))|((~GPR[`rdst][15])&(GPR[`rsrc1][15])&(~GPR[`rsrc2][15])) ;
+     begin
+     overflow = 1'b0;
+     end
+ 
+end
+
+
+
+
+
+ 
+ 
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+/////////////////////////////////////////////
+///////////reading program
+
+
+
+reg [3:0] pc; // Program counter to keep track of the instruction address
+  always @(posedge clk) begin
+    // Fetch the instruction from the instruction memory
+    IR <= inst_mem[pc];
+
+    // Increment the program counter to fetch the next instruction
+    pc <= pc + 1;
   end
-  
-  //////////////////
-  
-  
-  
-  
-  end
-     
- endmodule
+
+ 
+ 
+endmodule
+
+ 
+////////////////////////////////////////////////////////////////////////////
+ 
